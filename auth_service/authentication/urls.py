5070dfb76a4b9c5cb6ebefe9e3341dab5dadd933
nikitt <nikittaosipov@yandex.ru>
2025-02-01 16:58:25 +0400

    urls

---
+++ auth_service/authentication/urls.py
@@ -0,0 +1,25 @@
+from django.urls import path
+
+from auth_service.authentication.views import (
+    UserRegisterAPIView,
+    UserConfirmAPIView,
+    UserUpdateAPIView,
+    UserProfileAPIView,
+    UserListAPIView,
+    UserDestroyAPIView,
+)
+
+from django.contrib.auth.views import LogoutView
+app_name = "authentication"
+
+urlpatterns = [
+    path("login/", UserRegisterAPIView.as_view(), name="login"),
+    path("login/confirm/", UserConfirmAPIView.as_view(), name="confirm"),
+    path("logout/", LogoutView.as_view(), name="logout"),
+
+
+    path("", UserListAPIView.as_view(), name="user_list"),
+    path("<int:pk>/", UserProfileAPIView.as_view(), name="user_detail"),
+    path("<int:pk>/update/", UserUpdateAPIView.as_view(), name="user_update"),
+    path("<int:pk>/delete/", UserDestroyAPIView.as_view(), name="user_delete"),
+]
